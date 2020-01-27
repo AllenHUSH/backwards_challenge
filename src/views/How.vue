02660b2bac006e2b1764f1b391d5bdeef0cab9da
AllenHUSH <shunhanghu@foxmail.com>
2020-01-27 17:16:51 +0800

    add:添加了logo

--- src/views/How.vue
+++ src/views/How.vue
@@ -1,3 +1,29 @@
 <template>
-    <div></div>
+  <div>
+    <h1>规则</h1>
+    <p>你说，我猜。</p>
+
+    <p>三两好友更欢乐。</p>
+
+    <p>说不明白了，快看视频———</p>
+
+    <p>
+      <a
+        href="https://www.bilibili.com/video/av76976000"
+        target="blank"
+      >【倒放挑战】我们俩像弱智...</a>
+    </p>
+    <p>
+      <a
+        href="https://www.bilibili.com/video/av71818687"
+        target="blank"
+      >笑死！输了就穿奥特曼皮套，无敌羞耻！【小潮倒放挑战】</a>
+    </p>
+    <p>
+      <a
+        href="https://www.bilibili.com/video/av78622872"
+        target="blank"
+      >倒放挑战！地狱级难度！！...</a>
+    </p>
+  </div>
 </template>
